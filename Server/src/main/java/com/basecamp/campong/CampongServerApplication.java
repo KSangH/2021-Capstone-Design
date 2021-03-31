eccbcf3df104ccdcd620e42114a8a89b11ad042a
KSANGH <ooii1224@naver.com>
2021-03-31 15:20:03 +0900

    [서버] 서버 최초 생성

---
+++ Server/src/main/java/com/basecamp/campong/CampongServerApplication.java
@@ -0,0 +1,13 @@
+package com.basecamp.campong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampongServerApplication.class, args);
+    }
+
+}
