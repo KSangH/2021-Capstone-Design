eef94489b0753462d25821f69621b06f78194207
oreocube <oreocube13@gmail.com>
2021-04-27 12:11:33 +0900

    Add post method for signup

---
+++ Android/Campong/app/src/main/java/com/basecamp/campong/model/ResultSignup.kt
@@ -0,0 +1,4 @@
+package com.basecamp.campong.model
+
+class ResultSignup {
+}
--- Android/Campong/app/src/main/java/com/basecamp/campong/retrofit/RetrofitService.kt
+++ Android/Campong/app/src/main/java/com/basecamp/campong/retrofit/RetrofitService.kt
@@ -1,12 +1,32 @@
 package com.basecamp.campong.retrofit
 
-import retrofit2.http.GET
-import retrofit2.http.POST
+import com.basecamp.campong.model.ResultSignup
+import com.basecamp.campong.utils.API
+import retrofit2.Call
+import retrofit2.http.*
 
 interface RetrofitService {
 
     // @GET()
+//    fun getVillageFcst(
+//        @Query("base_date") base_date: String,
+//        @Query("base_time") base_time: String,
+//        @Query("nx") nx: Int,
+//        @Query("ny") ny: Int,
+//
+//        // fixed value
+//        @Query("serviceKey", encoded = true)
+//        @Query("dataType") dataType: String = "JSON"
+//    ): Call<>
 
     // @POST()
-
+    @FormUrlEncoded
+    @POST(API.USER_SIGN_UP)
+    fun requestSignup(
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("usernick") usernick: String,
+        @Field("phone") phone: String,
+        @Field("username") username: String
+    ): Call<ResultSignup>
 }
