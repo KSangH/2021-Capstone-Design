a1f38b776dd810c365c625112428f0111c217392
oreocube <oreocube13@gmail.com>
2021-05-02 10:28:53 +0900

    [Android] 화면 추가 SearchLocationActivity (미완)

---
+++ Android/Campong/app/src/main/java/com/basecamp/campong/SearchLocationActivity.kt
@@ -0,0 +1,11 @@
+package com.basecamp.campong
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_location)
+    }
+}
