8429d4e12dfa71493011a601b20a46d41091c968
oreocube <oreocube13@gmail.com>
2021-05-02 13:55:07 +0900

    [Android] Merge branch a100 to a200

--- Android/Campong/app/src/main/java/com/basecamp/campong/view/SearchActivity.kt
+++ Android/Campong/app/src/main/java/com/basecamp/campong/view/SearchActivity.kt
@@ -1,7 +1,9 @@
 package com.basecamp.campong.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.basecamp.campong.R
 
 class SearchActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+    }
+
+    fun goToFilter(view: View) {
+        val intent = Intent(applicationContext, FilterActivity::class.java)
+        startActivity(intent)
     }
 }
