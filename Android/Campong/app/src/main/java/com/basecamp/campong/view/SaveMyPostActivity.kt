4e88d11bc7091b1cb418a4bad0ba1da94360a035
jangtang2 <atoa04@naver,com>
2021-05-03 19:13:28 +0900

    [Android] Create SaveMyPostActivity
    - SaveMyPostActivity.kt와 activity_save_my_post.xml 생성
    - 디자인에 맞춰 activity_save_my_post.xml 코드 작성
    - 박스 형태의 EditText 적용을 위한 edit_text_box.xml 작성
    - add 아이콘 크기 수정
    - 이미지 버튼 xml 작성
    - activity_send_request.xml의 사진 크기 수정

---
+++ Android/Campong/app/src/main/java/com/basecamp/campong/view/SaveMyPostActivity.kt
@@ -0,0 +1,12 @@
+package com.basecamp.campong.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.basecamp.campong.R
+
+class SaveMyPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_my_post)
+    }
+}
