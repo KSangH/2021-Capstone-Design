47359db30f2ad14e8d9b24c8981c483288a39f34
jangtang2 <atoa04@naver,com>
2021-05-04 14:17:41 +0900

    [Android] Create ShowMyPostActivity
    - 메뉴창 여는 버튼에 사용하기 위한 점 3개 아이콘 (ic_more_vert) 추가
    - ShowMyPostActivity.kt와 activity_show_my_post.xml 생성
    - activity_show_my_post.xml 디자인에 맞게 작성
    - 카테고리 표시를 위한 칩 형태의 text view 작성

---
+++ Android/Campong/app/src/main/java/com/basecamp/campong/view/ShowMyPostActivity.kt
@@ -0,0 +1,12 @@
+package com.basecamp.campong.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.basecamp.campong.R
+
+class ShowMyPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_my_post)
+    }
+}
