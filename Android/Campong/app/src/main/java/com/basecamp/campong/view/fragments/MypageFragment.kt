51b310b8cb12ed05b7dee0cef199286a8a5a41a4
oreocube <oreocube13@gmail.com>
2021-05-09 14:53:37 +0900

    [Android] 마이페이지 프로필 정보 요청 추가

--- Android/Campong/app/src/main/java/com/basecamp/campong/view/fragments/MypageFragment.kt
+++ Android/Campong/app/src/main/java/com/basecamp/campong/view/fragments/MypageFragment.kt
@@ -8,11 +8,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.basecamp.campong.R
 import com.basecamp.campong.databinding.FragmentMypageBinding
+import com.basecamp.campong.retrofit.RetrofitManager
+import com.basecamp.campong.utils.API
 import com.basecamp.campong.view.EditProfileActivity
+import com.bumptech.glide.Glide
 
 class MypageFragment : Fragment(), View.OnClickListener {
 
     private var mBinding: FragmentMypageBinding? = null
+    private var image_id: Long? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +26,8 @@ class MypageFragment : Fragment(), View.OnClickListener {
         val binding = FragmentMypageBinding.inflate(inflater, container, false)
 
         mBinding = binding
+
+        getUserInfo()
 
         binding.goToEditProfileButton.setOnClickListener(this)
 
@@ -42,6 +48,30 @@ class MypageFragment : Fragment(), View.OnClickListener {
         when (v.id) {
             R.id.goToEditProfileButton -> {
                 goToEditProfile(v)
+            }
+        }
+    }
+
+    fun getUserInfo() {
+        RetrofitManager.instance.requestUserInfo { code, usernick, imageid ->
+            when (code) {
+                0 -> {
+                    if (mBinding != null) {
+                        if (usernick != null) {
+                            mBinding!!.nicknameTextView.text = usernick
+                        }
+                        if (imageid != null) {
+                            image_id = imageid
+                            val url = "${API.BASE_URL}/image/$image_id"
+
+                            Glide.with(this)
+                                .load(url)
+                                .centerCrop()
+                                .into(mBinding!!.profile)
+                        }
+                    }
+
+                }
             }
         }
     }
