0bb9a52097ea0afe29e300237dfaedadaf2322d4
jangtang2 <atoa04@naver,com>
2021-05-09 14:24:40 +0900

    [Android] Add Email Verification
    - 이메일 형식 확인하는 기능 추가
    - 이메일 형식에 맞지않으면 붉은 글씨, 형식에 부합하면 검정 글씨로 전환

--- Android/Campong/app/src/main/java/com/basecamp/campong/view/SignupActivity.kt
+++ Android/Campong/app/src/main/java/com/basecamp/campong/view/SignupActivity.kt
@@ -1,7 +1,10 @@
 package com.basecamp.campong.view
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -10,6 +13,7 @@ import com.basecamp.campong.R
 import com.basecamp.campong.databinding.ActivitySignupBinding
 import com.basecamp.campong.retrofit.RetrofitManager
 import com.basecamp.campong.utils.Constants.TAG
+import java.util.regex.Pattern
 
 class SignupActivity : AppCompatActivity() {
 
@@ -24,6 +28,23 @@ class SignupActivity : AppCompatActivity() {
         mBinding = ActivitySignupBinding.inflate(layoutInflater)
 
         setContentView(mBinding.root)
+
+        mBinding.emailEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // text가 변경된 후 호출
+                // s에는 변경 후의 문자열이 담겨 있다.
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // text가 변경되기 전 호출
+                // s에는 변경 전 문자열이 담겨 있다.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // text가 바뀔 때마다 호출된다.
+                correctEmail()
+            }
+        })
     }
 
     fun checkEmail(view: View) {
@@ -96,5 +117,21 @@ class SignupActivity : AppCompatActivity() {
         } else {
             Toast.makeText(applicationContext, "닉네임 중복확인이 필요합니다.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun correctEmail(): Boolean{
+
+        val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        var email = mBinding.emailEditText.text.toString().trim() //공백제거
+        val p = Pattern.matches(emailValidation, email) // 서로 패턴이 맞는가?
+        if (p) {
+            //이메일 형태가 정상일 경우
+            mBinding.emailEditText.setTextColor(Color.BLACK)
+            return true
+        } else {
+            mBinding.emailEditText.setTextColor(Color.RED)
+            return false
+        }
+
     }
 }
