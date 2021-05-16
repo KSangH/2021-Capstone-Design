e296721c07248f7ff8b0aadb86c9249fa9fb1385
oreocube <oreocube13@gmail.com>
2021-05-16 14:32:42 +0900

    [Android] QR 라이브러리 추가, QR 생성

--- Android/Campong/app/src/main/java/com/basecamp/campong/view/QrViewActivity.kt
+++ Android/Campong/app/src/main/java/com/basecamp/campong/view/QrViewActivity.kt
@@ -1,12 +1,37 @@
 package com.basecamp.campong.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.basecamp.campong.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.basecamp.campong.databinding.ActivityQrViewBinding
+import com.basecamp.campong.utils.Constants
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class QrViewActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityQrViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qr_view)
+
+        mBinding = ActivityQrViewBinding.inflate(layoutInflater)
+
+        setUpQR("https://www.naver.com")
+        setContentView(mBinding.root)
+    }
+
+    fun setUpQR(text: String) {
+        val multiFormatWriter = MultiFormatWriter()
+
+        try {
+            val bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200)
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.createBitmap(bitMatrix)
+            mBinding.qrimageview.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+            Log.e(Constants.TAG, e.toString())
+        }
     }
 }
